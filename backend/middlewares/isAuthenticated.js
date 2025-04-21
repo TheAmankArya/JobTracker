ea42ca19d38173b047f8ade8e2ebb352e4da8c32
Amank Arya <amankarya59@gmail.com>
2025-04-21 22:01:02 +0530

    new change

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,5 +1,9 @@
 import jwt from "jsonwebtoken";
 
+import dotenv from "dotenv";
+dotenv.config();
+
+
 const isAuthenticated = async (req, res, next) => {
     try {
         const token = req.cookies.token;
